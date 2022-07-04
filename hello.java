953a0b559b1fbd70a4d77a1ad2020ad1e0b6bae9
Harsh Vyas <harsh_vyas@persistent.com>
2022-07-04 15:35:24 +0530

    Second Commit

--- hello.java
+++ hello.java
@@ -4,5 +4,9 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Harsh this is a java file");
+        hello.printName("Harsh");
+    }
+    public static void printName(String name) {
+        System.out.println("Name is : "+name);
     }
 }
